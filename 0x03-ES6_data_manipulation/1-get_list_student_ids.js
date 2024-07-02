270c14ed6fcf1bf4a778e9970559ca296cf2c1be
efa07 <efatariku07@gmail.com>
2024-07-02 20:53:03 +0300

    function converted to arrow function

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,8 @@
-export default function getListStudentIds(students) {
+const getListStudentIds = (students) => {
   if (!Array.isArray(students)) {
     return [];
   }
   return students.map((student) => student.id);
-}
+};
+
+export default getListStudentIds;

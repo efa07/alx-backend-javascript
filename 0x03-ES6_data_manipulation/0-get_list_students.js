45fcddbb1d636bdab2d8b0cddff841ee0622ee91
efa07 <efatariku07@gmail.com>
2024-07-02 21:04:05 +0300

    typo error fixed at line 5

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,5 +2,5 @@ export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'james', location: 'Columbia' },
-    { id: 5, firstName: 'Alice', location: 'San Francisco' }];
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
 }

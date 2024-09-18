3804eba971512dd0352620b2575d746fdf9678d7
efa07 <efatariku07@gmail.com>
2024-09-18 19:23:20 +0300

    file created

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (input) => {
+  const name = input.toString().trim();
+
+  console.log(`Your name is: ${name}`);
+
+  console.log('This important software is now closing');
+
+  process.stdin.end();
+});
+
+process.stdin.on('end', () => {
+  process.exit();
+});

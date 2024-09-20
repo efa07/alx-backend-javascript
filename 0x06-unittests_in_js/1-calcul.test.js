c4af90db5c708e91ee44639e152e087764ad2cd7
efa07 <efatariku07@gmail.com>
2024-09-20 18:55:54 +0300

    file created

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 6 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+
+    it('should return 0 when inputs are -0.4 and 0.4', () => {
+      assert.strictEqual(calculateNumber('SUM', -0.4, 0.4), 0);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return -4 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+
+    it('should return 0 when inputs are 2.5 and 2.5', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 2.5, 2.5), 0);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return Error when dividing by 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
